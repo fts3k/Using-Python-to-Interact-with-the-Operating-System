c939972ba1e0ae103480d906a4d2369c302e93f3
Your Name <you@example.com>
2021-04-15 19:39:43 +0300

    Changing folders

--- 2. Basic Regex/exercise5_regex_in_action.py
+++
@@ -1,8 +0,0 @@
-import re
-def check_sentence(text):
-    result = re.search(r"^[A-Z][a-z ]*[\.\?!]$", text)
-    return result != None
-
-print(check_sentence("Is this a sentence?"))
-print(check_sentence("is this a sentence?"))
-print(check_sentence("Hello"))    
