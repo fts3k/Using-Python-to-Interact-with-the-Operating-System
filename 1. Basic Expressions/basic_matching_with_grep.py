e3d7126f631057878cc0d45a1d6f4a7ce8fff222
Your Name <you@example.com>
2021-04-15 19:21:56 +0300

    Updated python regexs

--- 1. Basic Expressions/basic_matching_with_grep.py
+++ 1. Basic Expressions/basic_matching_with_grep.py
@@ -1,12 +1,12 @@
 # Prints out the search string.
 
-grep 'filename' 'searchstring'
+#grep 'filename' 'searchstring'
 
 # grep is case sensitive
 
 # To search irregardless of the case
 
-grep  -i 'filename' 'searchstring'
+#grep  -i 'filename' 'searchstring'
 
 # . (dot) matches any character - a wildcard that can be replaced by any other character.
 # ^ -beginning of the line
