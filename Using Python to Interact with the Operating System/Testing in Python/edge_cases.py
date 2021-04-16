3d9ce83ccdaffca3826fa0efd95e50c4461b0ffb
Kevin Ojijo <oyes3@outlook.com>
2021-04-16 21:01:13 +0300

    building first tst

---
+++ Using Python to Interact with the Operating System/Testing in Python/edge_cases.py
@@ -0,0 +1,8 @@
+# We may give our code input that we don't want it to run into under normal operations.
+
+# e.g what will happen to our function if we give it an empty string.
+
+# EDGE CASES - inputs to our code that produce unexpected results, and are found at the extreme ends of the ranges of input we imagine our programs will typically work with.
+
+# Edge cases need special handling in order for our code to behave correctly.
+
